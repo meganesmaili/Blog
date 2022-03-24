051a43b176809b5b67e10e80b038ab98c7e8fc09
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-03-24 15:38:28 +0100

    JS try

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function navOnClick()
+{
+    var element = document.getElementById("menuWrapped");
+    var hamburgerButton = document.getElementById("menuHamburgerButton")
+    if (element.className === "unwrappMenu") {
+        element.classList.remove("unwrappMenu");
+        if (hamburgerButton.classList.contains("opened"))
+        {
+            hamburgerButton.classList.remove("opened")
+        }
+    } else {
+        element.classList.add("unwrappMenu");
+    } 
+}
 
+let vh = window.innerHeight
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+ window.addEventListener('resize', () => {
+    let vh = window.innerHeight
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }); 
